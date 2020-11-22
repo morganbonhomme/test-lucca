e92ca500c1d2ec0a1881547d7f6bd06dd423e375
morganbonhomme <bonhomme.morgan@gmail.com>
2020-11-22 12:16:21 +0100

    added sorting

--- src/app/expense-table/expense-table.component.ts
+++ src/app/expense-table/expense-table.component.ts
@@ -1,45 +1,54 @@
 import { ExpenseFormComponent } from './../expense-form/expense-form.component';
 import { ExpenseService } from '../services/expense.service';
 import {
+  AfterViewInit,
   Component,
-  EventEmitter,
-  Input,
   OnDestroy,
   OnInit,
-  Output,
   ViewChild,
 } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-expense-table',
   templateUrl: './expense-table.component.html',
   styleUrls: ['./expense-table.component.css'],
 })
-export class ExpenseTableComponent implements OnInit, OnDestroy {
+
+export class ExpenseTableComponent implements OnInit, OnDestroy, AfterViewInit {
   constructor(
     private expenseService: ExpenseService,
     public dialog: MatDialog
   ) {}
 
   dataSource = new MatTableDataSource();
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   displayedColumns: string[] = [
     'purchasedOn',
     'comment',
     'nature',
-    'originalAmount',
-    'originalCurrency',
-    'convertedAmount',
+    // 'originalAmount',
+    // 'originalCurrency',
+    // 'convertedAmount',
   ];
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.getExpenses();
+    this.dataSource.sort = this.sort;
+
   }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
 
   getExpenses() {
     this.expenseService.getExpenses().subscribe((response) => {
--- src/app/submodules/angular-material.module.ts
+++ src/app/submodules/angular-material.module.ts
@@ -22,6 +22,7 @@ const materialModules = [
   MatSelectModule,
   MatButtonModule,
   MatDialogModule,
+  MatSortModule
 ];
 
 @NgModule({
